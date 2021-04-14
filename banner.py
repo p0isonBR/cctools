5d30bac6ed13b465f95801826534e44f9f1b711d
p0ison <65384918+p0isonBR@users.noreply.github.com>
2021-04-14 00:20:10 -0300

    Add files via upload

---
+++ banner.py
@@ -0,0 +1,17 @@
+R = '\033[1;31m'
+B = '\033[1;34m'
+Y = '\033[1;33m'
+RT = '\033[0;1m'
+C = '\033[1;90m'
+
+cc = f"""
+{C}.-----------------------.
+|{RT} {R}elo{B}card        {C}.-----.{RT}{C}|{RT} Autor:   {C}p0isonBR
+{C}|{RT} {Y}Classic        {C}{C}|{RT}     {C}|{RT}{RT}{C}|{RT} GitHub:  {C}https://github.com/p0isonBR
+{C}|{RT}                {C}'-----'{RT}{C}|{RT} Email:   {C}poisonbr@pm.me
+{C}|{RT}                       {C}|{RT} Contato: {C}https://t.me/p0isonBR
+{C}|{RT} 5067 75xx xxxx xxxx   {C}|{RT}
+{C}|{RT} {C}04/2021{RT}               {C}|{RT} Gerador, validador e testador
+{C}|{RT} POISON BR      {R}credito{RT}{C}|{RT}        de cartoes elo!
+{C}'-----------------------'
+"""
---
+++ gateOne.py
@@ -0,0 +1,4 @@
+def code(gg):
+    print('Seu script aqui, retornando "True" (live) ou o print do erro')
+    input(gg + ' foi importada. perte enter para continuar.')
+    return True
