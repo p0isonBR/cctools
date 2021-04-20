8d741c1195d12277abe489010e7eaf89833610a7
p0ison <65384918+p0isonBR@users.noreply.github.com>
2021-04-20 02:07:24 -0300

    Update gateOne.py

--- gateOne.py
+++ gateOne.py
@@ -1,4 +1,4 @@
 def code(gg):
     print('Seu script aqui, retornando "True" (live) ou o print do erro')
-    input(gg + ' foi importada. perte enter para continuar.')
+    input(gg + ' foi importada. Aperte enter para continuar.')
     return True
